fd697c879a9363574e5f5a070dbeb8ba67a5775d
Oleg Shilov <shilov-1@yandex.ru>
2025-12-17 15:55:53 +0300

    init;

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import { Wrapper, Counter } from 'components';
+
+export const App = () => {
+  return (
+    <div>
+      <Wrapper>
+        <h1>
+          Counter
+        </h1>
+
+        <Counter />
+      </Wrapper>
+    </div>
+  );
+};
