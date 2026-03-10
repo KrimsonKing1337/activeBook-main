b8fc3afdb696cb3f1b2d7c999c5271050a362d13
Oleg Shilov <shilov-1@yandex.ru>
2026-03-10 11:05:08 +0300

    added processing wrong user agent situation;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,41 +1,63 @@
+import { useEffect, useState } from 'react';
+
 import { BooksWrapper, Title } from 'components';
 
 import 'styles/styles.scss';
 
 import * as styles from './App.scss';
+import { WrongUserAgent } from '../WrongUserAgent';
 
 export const App = () => {
+  const [isInApp, setIsInApp] = useState(false);
+
+  useEffect(() => {
+    const { userAgent } = navigator;
+
+    const isInApp =
+      /Telegram|FBAN|FBAV|Instagram|WhatsApp|Line|MicroMessenger/i.test(userAgent);
+
+    setIsInApp(isInApp);
+  }, []);
+
   return (
     <div className={styles.Wrapper}>
       <Title>
         ActiveBooks
       </Title>
 
-      <div className={styles.Desc}>
-        <p>
-          Живые книги &mdash; это текст, который звучит и&nbsp;дышит.
-          Музыка, анимация, световые и&nbsp;тактильные эффекты усиливают атмосферу, сохраняя главное &mdash; чтение.
-        </p>
+      {isInApp && (
+        <WrongUserAgent />
+      )}
 
-        <p>
-          Ниже &mdash; демонстрация формата на&nbsp;примере двух произведений.
-        </p>
+      {!isInApp && (
+        <>
+          <div className={styles.Desc}>
+            <p>
+              Живые книги &mdash; это текст, который звучит и&nbsp;дышит.
+              Музыка, анимация, световые и&nbsp;тактильные эффекты усиливают атмосферу, сохраняя главное &mdash; чтение.
+            </p>
 
-        <p>
-          Лучше всего ощущается на&nbsp;смартфоне.
-        </p>
+            <p>
+              Ниже &mdash; демонстрация формата на&nbsp;примере двух произведений.
+            </p>
 
-        <p>
-          Внимание: не работает во&nbsp;встроенных браузерах мессенджеров.
-        </p>
-      </div>
+            <p>
+              Лучше всего ощущается на&nbsp;смартфоне.
+            </p>
 
-      <BooksWrapper />
+            <p>
+              Внимание: не работает во&nbsp;встроенных браузерах мессенджеров.
+            </p>
+          </div>
 
-      <div className={styles.Footer}>
-        <p>Демонстрационный прототип.</p>
-        <p>ActiveBook engine © Олег Шилов</p>
-      </div>
+          <BooksWrapper />
+
+          <div className={styles.Footer}>
+            <p>Демонстрационный прототип.</p>
+            <p>ActiveBook engine © Олег Шилов</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/components/WrongUserAgent/WrongUserAgent.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import * as styles from './WrongUserAgent.scss';
+
+export const WrongUserAgent = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const buttonClickHandler = () => {
+    setIsCopied(true);
+  };
+
+  const buttonLabel = isCopied ? 'Скопировано' : 'Скопировать ссылку';
+
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.Desc}>
+        Откройте, пожалуйста, в браузере (здесь работает некорректно)
+      </div>
+
+      <div className={styles.Button} onClick={buttonClickHandler}>
+        {buttonLabel}
+      </div>
+    </div>
+  );
+};
