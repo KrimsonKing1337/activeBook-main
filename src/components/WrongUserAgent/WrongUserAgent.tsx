0dae0bc19b5fd66fd4a7ac1a960b3c357dab6e5d
Oleg Shilov <shilov-1@yandex.ru>
2026-03-10 13:20:31 +0300

    navigator clipboard did not work, made fallback;

--- src/components/WrongUserAgent/WrongUserAgent.tsx
+++ src/components/WrongUserAgent/WrongUserAgent.tsx
@@ -1,26 +1,14 @@
-import { useState } from 'react';
-
 import * as styles from './WrongUserAgent.scss';
 
 export const WrongUserAgent = () => {
-  const [isCopied, setIsCopied] = useState(false);
-
-  const buttonClickHandler = () => {
-    setIsCopied(true);
-
-    navigator.clipboard.writeText('https://active-books.ru');
-  };
-
-  const buttonLabel = isCopied ? 'Скопировано' : 'Скопировать ссылку';
-
   return (
     <div className={styles.Wrapper}>
       <div className={styles.Desc}>
-        Откройте, пожалуйста, в браузере (здесь работает некорректно)
+        Пожалуйста, скопируйте и откройте в браузере (здесь работает некорректно):
       </div>
 
-      <div className={styles.Button} onClick={buttonClickHandler}>
-        {buttonLabel}
+      <div className={styles.Link}>
+        https://active-books.ru
       </div>
     </div>
   );
